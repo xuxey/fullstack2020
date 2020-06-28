e8e5072a1229ad1ab8ad7e2ed9930d12d82dcaa7
ThatXuxe <obuoy1804@gmail.com>
2020-06-28 15:01:53 +0530

    Finish till exercise 7.7

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import {Link, Route, Switch, useHistory, useRouteMatch} from "react-router-dom"
+import {useField} from "./hooks"
 
 const Menu = () => {
     const padding = {
@@ -62,17 +63,16 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-    const [content, setContent] = useState('')
-    const [author, setAuthor] = useState('')
-    const [info, setInfo] = useState('')
     const history = useHistory()
-
+    const content = useField('text')
+    const author = useField('text')
+    const info = useField('text')
     const handleSubmit = (e) => {
         e.preventDefault()
         props.addNew({
-            content,
-            author,
-            info,
+            content: content.value,
+            author: author.value,
+            info: info.value,
             votes: 0
         })
         history.push('/')
@@ -85,17 +85,23 @@ const CreateNew = (props) => {
             <form onSubmit={handleSubmit}>
                 <div>
                     content
-                    <input name='content' value={content} onChange={(e) => setContent(e.target.value)}/>
+                    <input name='content' {...content.inputs}/>
                 </div>
                 <div>
                     author
-                    <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)}/>
+                    <input name='author' {...author.inputs}/>
                 </div>
                 <div>
                     url for more info
-                    <input name='info' value={info} onChange={(e) => setInfo(e.target.value)}/>
+                    <input name='info' {...info.inputs}/>
                 </div>
-                <button>create</button>
+                <button onClick={() => {
+                    content.reset();
+                    author.reset();
+                    info.reset()
+                }}>Reset
+                </button>
+                <button type="submit">Create</button>
             </form>
         </div>
     )
